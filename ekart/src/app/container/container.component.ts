6a804a2eb55e18cb21c6d4506e922ef4a79d7f22
Akshat Agarwal <48678480+akshatj21@users.noreply.github.com>
2024-04-16 05:32:25 +0000

    added product-list & search

---
+++ ekart/src/app/container/container.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css']
+})
+export class ContainerComponent {
+  // addToCart:number = 0;
+  // product = {
+  //   name: 'iPhone X',
+  //   price: 789,
+  //   color: 'Black',
+  //   discount: 8.5,
+  //   inStock: 10,
+  //   pImage: '/assets/img/iphone.png'
+  // }
+
+  // getDiscountedPrice() {
+  //   return this.product.price - (this.product.price * this.product.discount / 100)
+  // }
+
+  // decrementCartValue(){
+  //   if(this.addToCart > 0){
+  //     this.addToCart--;
+  //   }
+  // }
+
+  // incrementCartValue(){
+  //   if(this.addToCart < this.product.inStock){
+  //     this.addToCart++;
+  //   }
+  // }
+
+  
+}
